e7e246504cfa788a12305ffa4005ab60ab8cfeb5
rohansarakinti <sarakintirohan@gmail.com>
2023-09-30 12:24:04 -0500

    commit message

--- client/App.js
+++ client/App.js
@@ -2,109 +2,14 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View, Image, Alert, Pressable} from 'react-native'
 import { useState, useEffect } from 'react'
 import { useFonts } from 'expo-font'
+import LandingPage from "./screens/landingPage"
+import LoginPage from './screens/loginPage'
+import CreateAccountPage from './screens/createAccountPage'
 
 // Define the main App component
 export default function App() {
-  // Load custom fonts using the 'useFonts' hook
-  const [fontsLoaded] = useFonts({
-    'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
-    'Poppins-SemiBold' : require('./assets/fonts/Poppins-SemiBold.ttf'),
-    'Poppins-Medium' : require('./assets/fonts/Poppins-Medium.ttf')
-  })
-  if (!fontsLoaded) {
-    return null
-  } else {
-      
-    return (
-      <View>
-        <Image style={styles.logo_title} source={require('./assets/Intellispend_Logo.png')} />
-        <View style={styles.line}></View>
-        <Pressable
-        onPress={() => {
-          Alert.alert('Log In button pressed') 
-        }}
-        style={({pressed}) => [
-          {
-            backgroundColor: pressed ? '#5BC641' : '#72EC55' 
-          }, styles.login_button
-        ]}
-        >
-          <Text style={styles.login_button_text}>Log In</Text>
-        <Image style={{position:'absolute', top:'20%', left:'70%'}}source={require('./assets/login_icon.png')}/>
-        </Pressable>
-
-        <Pressable
-        onPress={() => {
-          Alert.alert('Sign Up button pressed') 
-        }}
-        style={({pressed}) => [
-          {
-            backgroundColor: pressed ? '#5BC641' : '#72EC55' 
-          }, styles.signup_button
-        ]}
-        >
-          <Text style={styles.signup_button_text}>Sign Up</Text>
-        <Image style={{position:'absolute', top:'25%', left:'68%'}}source={require('./assets/signup_icon.png')}/>
-        </Pressable>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
+  return(
+    <CreateAccountPage />
+  )
 }
-
-// Define styles using StyleSheet.create for the CSS section
-const styles = StyleSheet.create({
-  logo_title: {
-    width: '100%', 
-    height: '50%',
-    marginLeft:'auto', 
-    marginRight:'auto',
-    top: '20%'
-  },
-  line: {
-    top: '20%',
-    width: '70%',
-    borderBottomColor: '#239D54',
-    borderBottomWidth: '2%',
-    marginLeft: 'auto',
-    marginRight: 'auto'
-  },
-  login_button: { 
-    width: '50%', 
-    height: '10%', 
-    marginLeft: 'auto', 
-    marginRight:'auto', 
-    top:'30%', 
-    borderRadius: 10, 
-    shadowColor: '#171717', 
-    shadowOffset: { width: 0, height: 4}, 
-    shadowOpacity: 0.2, 
-    shadowRadius: 2,
-  },
-  login_button_text: {
-    fontFamily: 'Poppins-Medium', 
-    left:'21%', 
-    marginTop:'auto', 
-    marginBottom: 'auto', 
-    fontSize: 19,
-  },
-  signup_button: {
-    width: '50%', 
-    height: '10%', 
-    marginLeft: 'auto', 
-    marginRight:'auto', 
-    top:'40%', 
-    borderRadius: 10, 
-    shadowColor: '#171717', 
-    shadowOffset: { width: 0, height: 4}, 
-    shadowOpacity: 0.2, 
-    shadowRadius: 2,
-  },
-  signup_button_text: {
-    fontFamily: 'Poppins-Medium', 
-    left:'17%', 
-    marginTop:'auto', 
-    marginBottom: 'auto', 
-    fontSize: 19,
-  }
-});
+;
---
+++ client/screens/loginPage.js
@@ -0,0 +1,103 @@
+import {Text, View, StyleSheet, StatusBar, TextInput, Pressable} from 'react-native';
+import React from 'react'
+import { useFonts } from '@expo-google-fonts/poppins';
+
+const loginPage = () => {
+    const [fontsLoaded] = useFonts({
+      'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+      'Poppins-SemiBold' : require('../assets/fonts/Poppins-SemiBold.ttf'),
+      'Poppins-Medium' : require('../assets/fonts/Poppins-Medium.ttf')
+    })
+    if (!fontsLoaded) {
+      return null
+    } else {
+        
+      return (
+        <View style={styles.container}>
+          <Text style={styles.logInTitle} >Log In</Text>
+          <View style={styles.line}></View>
+          <TextInput style={styles.textInput} placeholder="Password"/>
+          <Pressable
+          onPress={() => {
+            Alert.alert('Log In button pressed') 
+          }}
+          style={({pressed}) => [
+            {
+              backgroundColor: pressed ? '#5BC641' : '#72EC55' 
+            }, styles.passwordSubmit
+          ]}
+          >
+          <Text style={styles.passwordSubmitText}>Submit</Text>
+          </Pressable>
+          <Text style={styles.forgotPasswordText}>forgot password?</Text>
+          <StatusBar style="auto" />
+        </View>
+      );
+    }
+}
+  
+const styles = StyleSheet.create({
+  container:{
+    alignItems: 'center',
+    padding: '10%',
+    backgroundColor: 'white',
+
+  },
+  logInTitle: {
+    top: '300%',
+    fontSize: 48,
+    fontFamily: 'Poppins-SemiBold',
+    color: '#333',
+    fontWeight: 600,
+  },
+  line: {
+    top: '350%',
+    width: '80%',
+    borderBottomColor: '#239D54',
+    borderBottomWidth: '2%',
+  },
+  textInput:{
+    paddingHorizontal: '10%',
+    position:'absolute',
+    backgroundColor: '#72EC55',
+    width: '90%',
+    height: '75%',
+    borderRadius: 10,
+    top: '570%',
+    color:"#239D54",
+    fontFamily: 'Poppins-Regular',
+    fontSize: '15%',
+    borderColor: "#239D54",
+    borderWidth: 3,
+
+  },
+  forgotPasswordText: {
+    position:'absolute',
+    color: "#239D54",
+    fontFamily : "Poppins-SemiBold",
+    top: '850%',
+    fontSize: '20%'
+
+  },
+  passwordSubmit: {
+    position: 'absolute',
+    width: '50%', 
+    height: '70%',
+    top: "710%",  
+    borderRadius: 10, 
+    shadowColor: '#171717', 
+    shadowOffset: { width: 0, height: 4}, 
+    shadowOpacity: 0.2, 
+    shadowRadius: 2,
+    alignItems: 'center'
+  },
+  passwordSubmitText: {
+    fontFamily: 'Poppins-Medium', 
+    marginTop:'auto', 
+    marginBottom: 'auto', 
+    fontSize: 19,
+  }
+})
+
+
+export default loginPage
