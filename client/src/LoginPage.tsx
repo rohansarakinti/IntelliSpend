a848308d99843ab69e9ddbafcf0c50c7ce0ceda3
rohansarakinti <sarakintirohan@gmail.com>
2023-10-22 01:06:17 -0500

    LoginPage(In Progress)

---
+++ client/src/LoginPage.tsx
@@ -0,0 +1,20 @@
+import {  } from 'react'
+
+import './LoginPage.css'
+
+function LoginPage() {
+
+  return (
+    <>
+        <div className='animated_gradient'>
+            <div className='box'>
+                <div className='login_container'>
+                    
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default LoginPage
