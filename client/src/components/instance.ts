a3f6600ff3c1023b9b00bd894fd99be1bb030a14
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-10-31 20:57:53 -0500

    updated baseURL

--- client/src/components/instance.ts
+++ client/src/components/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const client = axios.create({
-    baseURL: "https://intellispend.uc.r.appspot.com"
+    baseURL: "https://intellispend.onrender.com/"
 })
 
 export default client
